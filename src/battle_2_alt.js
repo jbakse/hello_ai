8526a94fbcfb3b412ad4905c19cd622e6e619213
Justin Bakse <justin@tsfim.com>
2023-11-16 15:20:18 -0600

    add battle_2_alt

---
+++ src/battle_2_alt.js
@@ -0,0 +1,189 @@
+import chalk from "chalk";
+import dedent from "dedent";
+
+import { say, ask, gptFunction, inspect, end } from "../shared.js";
+
+/////////////////////////////////////////////////////////////////
+// GAME CODE
+
+const game_data = {
+  player_hp: 10,
+  player_mp: 1,
+  enemy_hp: 10,
+};
+
+function attack() {
+  const damage = randomInt(0, 3);
+  game_data.enemy_hp -= damage;
+  let summary = `Player attacks: ${damage}/3 damage. `;
+  summary += counter();
+  summary += sitRep();
+  return summary;
+}
+
+function magic() {
+  if (game_data.player_mp < 1) {
+    return "no mana. ";
+  }
+  const damage = randomInt(3, 5);
+  game_data.enemy_hp -= damage;
+  game_data.player_mp -= 1;
+  let summary = `Player attacks: ${damage}/3 magical damage. `;
+  summary += counter();
+  summary += sitRep();
+  return summary;
+}
+
+function counter() {
+  if (game_data.enemy_hp <= 0) {
+    return "Enemy is killed! ";
+  }
+  const damage = randomInt(0, 3);
+  game_data.player_hp -= damage;
+  let summary = `Enemy attacks: ${damage}/3 damage. `;
+  return summary;
+}
+
+function sitRep() {
+  return JSON.stringify(game_data);
+}
+
+/////////////////////////////////////////////////////////////////
+// GPT CONFIG
+
+const tools = [
+  {
+    type: "function",
+    function: {
+      name: "attack",
+      description: "Attack the enemy with a weapon",
+      parameters: {
+        type: "object",
+        properties: {},
+        required: [],
+      },
+    },
+  },
+  {
+    type: "function",
+    function: {
+      name: "magic",
+      description: "Attack the enemy with magic",
+      parameters: {
+        type: "object",
+        properties: {},
+        required: [],
+      },
+    },
+  },
+];
+
+const availableFunctions = {
+  attack,
+  magic,
+};
+
+const messages = [
+  {
+    role: "system",
+    content: dedent`
+      You assist in running a role-playing text adventure game. You take user commands, call functions to update the game state, and narrate the results.
+      
+      The player is engaged in battle and can't pursue other activities until it concludes. 
+      
+      - Narrate only the latest turn.
+      - Avoid using numbers in your narration. 
+      - Be brief. 
+
+      The player and the enemy both start with their maximum health: 10.
+      If the user looks at the enemy describe its physical condition and health.
+      
+      The player has two functions available: attack and magic. 
+
+      The player knows the following spells: firebolt
+
+      The room doesn't have any items.
+      The player doesn't have any items.
+
+      When calling functions, choose only the best single function. Never call more than one function at a time.
+      
+      If a tool returns "unknown function" let the player know they can't do the thing they tried.
+      `,
+  },
+];
+
+/////////////////////////////////////////////////////////////////
+// GAME LOOP
+
+async function game() {
+  // start with some introductory text
+  const introText = "A wild slime appears!";
+  messages.push({ role: "assistant", content: introText });
+  say(`\n${introText}`);
+
+  // the main game loop
+  while (game_data.player_hp > 0 && game_data.enemy_hp > 0) {
+    // show the current game state
+    inspect(game_data);
+
+    // ask the user for their next command
+    let command = await ask();
+    messages.push({ role: "user", content: command });
+
+    // use GPT to respond to command
+    let responseMessage = await gptFunction({
+      messages,
+      tools,
+      max_tokens: 256,
+    });
+    messages.push(responseMessage);
+
+    // if GPT calls tools, handle them
+    if (responseMessage.tool_calls) {
+      handleToolCalls(responseMessage.tool_calls);
+      responseMessage = await gptFunction({
+        messages,
+        max_tokens: 256,
+      });
+      messages.push(responseMessage);
+    }
+
+    // show user response
+    say(responseMessage.content);
+  }
+}
+
+function handleToolCalls(tool_calls) {
+  for (const tool_call of tool_calls) {
+    const functionName = tool_call.function.name;
+    const response = availableFunctions[functionName]?.() || "unknown function";
+    say(chalk.blue(`${functionName}()`));
+    messages.push({
+      tool_call_id: tool_call.id,
+      role: "tool",
+      name: functionName,
+      content: response,
+    });
+  }
+}
+
+try {
+  await game();
+} catch (e) {
+  console.error(chalk.red("\nWell, something went wrong."));
+  console.error(e);
+} finally {
+  const shouldLog = await ask("\nLog messages? (y/n) ");
+  if (shouldLog === "y") {
+    inspect(messages);
+  }
+  console.log("\n--end--\n");
+  end();
+}
+
+/////////////////////////////////////////////////////////////////
+// UTIL
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
