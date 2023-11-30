fc9111d7a7b6413a8ad577d3e249bade8d98679f
Justin Bakse <justin@tsfim.com>
2023-11-29 20:37:27 -0600

    Add chdir to current directory to hello_deno

--- src/deno/hello_deno/index.js
+++ src/deno/hello_deno/index.js
@@ -1,21 +1,16 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-
 import { gptPrompt } from "./gpt.js";
 
+// change working directory to directory of this file
+const dirName = new URL(".", Deno.mainModule).pathname;
+Deno.chdir(dirName);
+
 const app = new Application();
-
 const router = new Router();
-// router.get("/", (ctx) => {
-//   ctx.response.body = "Hello World!";
-// });
-router.get("/gpt", async (ctx) => {
-  const result = await gptPrompt("What rhymes with orange? Be breif!");
-  ctx.response.body = result;
-});
 
-app.use(async (context, next) => {
+// Serve static files from public directory
+async function staticServer(context, next) {
   try {
-    console.log("cwd", Deno.cwd());
     await context.send({
       root: `${Deno.cwd()}/public`,
       index: "index.html",
@@ -23,10 +18,16 @@ app.use(async (context, next) => {
   } catch {
     await next();
   }
-});
+}
+app.use(staticServer);
 
+// API routes
+router.get("/api/gpt", async (ctx) => {
+  const result = await gptPrompt("What rhymes with orange? Be breif!");
+  ctx.response.body = result;
+});
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log("Listening on http://localhost:8000");
+console.log(`Listening on http://localhost:8000`);
 await app.listen({ port: 8000 });
