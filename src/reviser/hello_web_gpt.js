879505952d67e3f17582bb31e6705df71c063490
Justin Bakse <justin@tsfim.com>
2024-02-14 14:30:25 -0600

    add Reviser example

---
+++ src/reviser/hello_web_gpt.js
@@ -0,0 +1,25 @@
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { gptPrompt } from "../shared/openai.js";
+import { exitSignal, staticServer } from "../shared/server.js";
+
+import { Chalk } from "npm:chalk@5";
+const chalk = new Chalk({ level: 1 });
+
+const app = new Application();
+const router = new Router();
+
+// API routes
+router.get("/api/gpt", async (ctx) => {
+  const prompt = ctx.request.url.searchParams.get("prompt");
+  const shortPrompt = prompt.slice(0, 1024);
+  const result = await gptPrompt(shortPrompt, { max_tokens: 1024 });
+  ctx.response.body = result;
+});
+
+app.use(staticServer);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(chalk.green("\nListening on http://localhost:8000"));
+
+await app.listen({ port: 8000, signal: exitSignal });
---
+++ src/reviser/public/main.js
@@ -0,0 +1,62 @@
+console.log("Hello, main.js");
+
+const revisionTab = document.getElementById("revision-tab");
+const diffTab = document.getElementById("diff-tab");
+
+const originalInput = document.getElementById("original");
+const instructionInput = document.getElementById("instruction");
+const revisionDiv = document.getElementById("revision");
+const diffDiv = document.getElementById("diff");
+
+const submitButton = document.getElementById("submit");
+
+async function onSubmit() {
+  const instruction = instructionInput.value;
+  const version1 = originalInput.value;
+  const prompt =
+    `revise the following text. return only the revised text. make no changes to the text unless the change is indicated by the following instructions: ${instruction}. \n\n${version1}`;
+
+  revisionDiv.innerHTML = "loading...";
+  diffDiv.innerHTML = "loading...";
+
+  const version2 = await fetch(`/api/gpt?prompt=${prompt}`)
+    .then((response) => response.text());
+
+  revisionDiv.innerHTML = version2;
+  updateDiff(version1, version2);
+}
+
+function updateDiff(version1, version2) {
+  const diff = Diff.diffWords(version1, version2);
+  diffDiv.innerHTML = "";
+  diff.forEach((part) => {
+    const className = part.added ? "added" : part.removed ? "removed" : "";
+    const span = document.createElement("span");
+    span.className = className;
+    span.appendChild(document.createTextNode(part.value));
+    diffDiv.appendChild(span);
+  });
+}
+
+revisionTab.addEventListener("click", () => {
+  console.log("revision-tab");
+  revisionDiv.classList.remove("hidden");
+  diffDiv.classList.add("hidden");
+  revisionTab.classList.add("bg-gray-600");
+  diffTab.classList.remove("bg-gray-600");
+});
+
+diffTab.addEventListener("click", () => {
+  console.log("diff-tab");
+  diffDiv.classList.remove("hidden");
+  revisionDiv.classList.add("hidden");
+  diffTab.classList.add("bg-gray-600");
+  revisionTab.classList.remove("bg-gray-600");
+});
+
+submitButton.addEventListener("click", onSubmit);
+
+originalInput.value =
+  `It was a dark and stormy night; the rain fell in torrents—except at occasional intervals, when it was checked by a violent gust of wind which swept up the streets (for it is in London that our scene lies), rattling along the housetops, and fiercely agitating the scanty flame of the lamps that struggled against the darkness.`;
+
+instructionInput.value = `add adjectives`;
